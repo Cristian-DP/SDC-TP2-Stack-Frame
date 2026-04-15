2caf2f5fa96a21773ac7da7fa813a45ac498167f
Cristian-DP <cristian.pereyra@mi.unc.edu.ar>
2026-04-14 22:53:32 -0300

    flask funcional

---
+++ iteracion-1/middleware.c
@@ -0,0 +1,4 @@
+/*
+	Los datos de consulta realizados deben ser entregados a un programa en C (capa intermedia) hagan los cálculos de conversión de float a enteros
+    y devuelva el índice de un país como Argentina u otro sumando uno (+1). 
+*/
